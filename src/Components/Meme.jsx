4b45f45695f0a7902f57ecbd6868b6496e977716
Thomassaiag <thomas.saiag@gmail.com>
2023-07-27 16:14:05 +0200

    product complete

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -26,12 +26,21 @@ export default function Meme(){
 
     
 
-    React.useEffect(()=>{
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res=> res.json())
-        .then(data=>setAllMemes(data.data.memes))
-    },[])
+    // React.useEffect(()=>{
+    //     fetch("https://api.imgflip.com/get_memes")
+    //     .then(res=> res.json())
+    //     .then(data=>setAllMemes(data.data.memes))
+    // },[])
 
+
+    React.useEffect(()=>{
+        async function getMemes(){
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data= await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    },[])
 
 
 
